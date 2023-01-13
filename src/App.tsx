6d87387156baae02ebea3be1d43cc6540e4c47f5
Jose Maria Ureña Torres <chematorres@Joses-MacBook-Air.local>
2023-01-12 21:41:59 -0600

    create the navbar file ando other pages for the project...also install firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Main from './Pages/Main'
+import Login from './Pages/Login'
+import Navbar from './Components/NavBar/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path='/' element={<Main/>}/>
+          <Route path='login' element={<Login/>}/>
+          <Route path='*' element={<Main/>}/>
+
+        </Routes>
+
+      </BrowserRouter>
     </div>
   )
 }
