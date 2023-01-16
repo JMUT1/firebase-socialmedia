b63f1cef895f56e4fd582f4c6cb00a9e31d63419
Jose Maria Ureña Torres <chematorres@Joses-MacBook-Air.local>
2023-01-16 10:41:12 -0600

    creating the like button so we can get the data of who like a comment

--- src/Pages/Main/post.tsx
+++ src/Pages/Main/post.tsx
@@ -1,15 +1,37 @@
-import {Post as Ipost} from "./Main"
-
+import { addDoc, collection } from "firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../../config/firebase";
+import { Post as Ipost } from "./Main";
 
 interface Props {
-    post: Ipost
+  post: Ipost;
 }
 
 const Post = (props: Props) => {
-    const {post} = props
-  return (
-    <div>post</div>
-  )
+  const { post } = props;
+  const [user] = useAuthState(auth)
+
+    const likesRef = collection(db, "likes")
+
+const addLike = async()=>{
+    await addDoc(likesRef, {userId: user?.uid , postId: post.id})
 }
 
-export default Post
+
+  return (
+    <div>
+      <div>
+        <h1>{post.title}</h1>
+      </div>
+      <div>
+        <p>{post.description}</p>
+      </div>
+      <div>
+        <p>@{post.username}</p>
+        <button onClick={addLike}>&#128077;</button>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
